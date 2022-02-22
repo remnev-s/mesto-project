95a0bcf5bcd76dc12a76691171cf64ac64957d77
Stas Remnev <remnev.stas@gmail.com>
2022-02-22 16:30:56 +0300

    fix: page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,7 @@
 import './index.css';
 import { enableValidation } from '../components/validate.js';
-import {
-  createCard,
-  handlerCardFormSubmit,
-  popupSaveBtn,
-} from '../components/card.js';
-import {
-  openPopup,
-  closePopup,
-  popup,
-  infoEdit,
-  infoPopupClose,
-  cardsPopupOpen,
-  cardsPopupClose,
-  imagePopupClose,
-  cardsPopup,
-  infoPopup,
-  nameInput,
-  jobInput,
-  infoName,
-  infoDescription,
-} from '../components/modal.js';
+import { createCard } from '../components/card.js';
 
-// ПЕРЕМЕННЫЕ ДЛЯ ПОПАПА ОТКРЫТИЯ КАРТИНОК
-const popupImg = document.querySelector('.popup_image');
-
-// ПЕРЕМЕННЫ ДЛЯ ФОРМ
-const formElement = document.querySelector('.popup__form');
-const saveBtnCard = document.querySelector('.popup__save-btn_add_card'); // кнопка создать
-
-/* ---------------------------------------------------------------------------- */
-const titleInput = document.querySelector('.popup__input-title'); // переменная названия картинки
-const linkInput = document.querySelector('.popup__input-link'); // переменная ссылки картинки
-
-// ФОРМА РЕДАКТИРОВАНИЯ ПРОФИЛЯ
-function getFormSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
-  infoName.textContent = nameInput.value;
-  infoDescription.textContent = jobInput.value;
-
-  closePopup(infoPopup);
-  formElement.reset();
-  popupSaveBtn.classList.add('popup__save-btn_inactive');
-  popupSaveBtn.setAttribute('disabled', true);
-}
-formElement.addEventListener('submit', getFormSubmitHandler);
 /* ---------------------------------------------------------------------------- */
 
 enableValidation({
